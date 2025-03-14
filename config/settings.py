8309608f451e220853f7d0190913888ab61f3cbd
youngjeaMo <tony9595@naver.com>
2025-03-14 14:48:50 +0900

    commit

--- config/settings.py
+++ config/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / "templates"
+        ],  # DIRS 에 지정이 없을경우 => 각 앱의 하위폴더에 templates를 생성해줘야 한다.
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- pybo/views.py
+++ pybo/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from pybo.models import Question
+
 # Create your views here.
+
+
+def index(requst):
+    question_list = Question.objects.order_by("-create_date")
+    context = {"question_list": question_list}
+    return render(requst, "pybo/question_list.html", context)

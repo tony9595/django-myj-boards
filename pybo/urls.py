ad1a6d06077945c37b09ab24372085cd80cb40a6
youngjeaMo <tony9595@naver.com>
2025-03-17 10:18:55 +0900

    질문 상세 페이지 추가 및 URL 경로 수정

--- pybo/urls.py
+++ pybo/urls.py
@@ -5,4 +5,5 @@ from pybo import views
 
 urlpatterns = [
     path("", views.index),
+    path("<int:question_id>/", views.detail),
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -10,3 +10,9 @@ def index(requst):
     question_list = Question.objects.order_by("-create_date")
     context = {"question_list": question_list}
     return render(requst, "pybo/question_list.html", context)
+
+
+def detail(requst, question_id):
+    question = Question.objects.get(id=question_id)
+    context = {"question": question}
+    return render(requst, "pybo/question_detail.html", context)

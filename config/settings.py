836f7fce2a5851d0f7ba0b216d2dd00e652b326c
youngjeaMo <tony9595@naver.com>
2025-03-17 14:06:18 +0900

    commit

--- config/settings.py
+++ config/settings.py
@@ -118,7 +118,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+
+# 스태틱(static) 디렉터리
+# URL 상의 스태틱 폴더를 ,로컬의 파일 폴더와 매칭 시키는 부분
+
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

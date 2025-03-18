d492f840fb8d927603b6e74969adcf9ad0891871
youngjeaMo <tony9595@naver.com>
2025-03-18 11:13:02 +0900

    commit

--- pybo/views.py
+++ pybo/views.py
@@ -33,5 +33,15 @@ def answer_create(request, question_id):
 
 
 def question_create(request):
-    form = QuestionForm()
-    return render(request, "pybo/question_form.html", {"form": form})
+
+    if request.method == "POST":
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect("pybo:index")
+
+    else:
+        form = QuestionForm()
+        return render(request, "pybo/question_form.html", {"form": form})

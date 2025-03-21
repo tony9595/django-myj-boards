dc2d3448d53151f41a0993ea497f58020aaabc0c
youngjeaMo <tony9595@naver.com>
2025-03-21 10:01:56 +0900

    commit

--- common/urls.py
+++ common/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
         name="login",
     ),
     path("logout/", views.logout_view, name="logout"),
+    path("signup/", views.signup, name="signup"),
 ]
--- common/views.py
+++ common/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login, authenticate
+
+from common.forms import UserForm
 
 # Create your views here.
 
@@ -7,3 +9,21 @@ from django.contrib.auth import logout
 def logout_view(request):
     logout(request)
     return redirect("index")
+
+
+def signup(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            raw_password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=raw_password)  # 사용자 인증
+            login(request, user)  # 로그인
+
+            return redirect("index")
+
+    else:
+        form = UserForm()
+
+    return render(request, "common/signup.html", {"form": form})

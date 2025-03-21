873e5ce1c644ebd189ae6b4f3f37466688b2c5d2
youngjeaMo <tony9595@naver.com>
2025-03-21 11:49:29 +0900

    commit

--- common/views.py
+++ common/views.py
@@ -12,12 +12,15 @@ def logout_view(request):
 
 
 def signup(request):
+
     if request.method == "POST":
         form = UserForm(request.POST)
+
         if form.is_valid():
             form.save()
+
             username = form.cleaned_data.get("username")
-            raw_password = form.cleaned_data.get("password")
+            raw_password = form.cleaned_data.get("password1")
             user = authenticate(username=username, password=raw_password)  # 사용자 인증
             login(request, user)  # 로그인
 

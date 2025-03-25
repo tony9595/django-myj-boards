c97637a71c1ef3e8b7ee2dea471c2e00dda8fa9a
youngjeaMo <tony9595@naver.com>
2025-03-25 11:08:17 +0900

    commit

--- pybo/urls.py
+++ pybo/urls.py
@@ -20,4 +20,14 @@ urlpatterns = [
         views.question_delete,
         name="question_delete",
     ),
+    path(
+        "answer/modify/<int:answer_id>/",
+        views.answer_modify,
+        name="answer_modify",
+    ),
+    path(
+        "answer/delete/<int:answer_id>/",
+        views.answer_delete,
+        name="answer_delete",
+    ),
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -148,3 +148,38 @@ def question_delete(request, question_id):
         return redirect("pybo:detail", question_id=question_id)
     question.delete()
     return redirect("pybo:index")
+
+
+# dev_18
+@login_required(login_url="common:login")
+def answer_modify(request, answer_id):
+
+    answer = get_object_or_404(Answer, pk=answer_id)
+
+    if request.user != answer.author:
+        messages.error(request, "수정 권한이 없습니다.")
+        return redirect("pybo:detail", question_id=answer.question.id)
+
+    if request.method == "POST":
+        form = AnswerForm(request.POST, instance=answer)
+
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.modify_date = timezone.now()
+            answer.save()
+            return redirect("pybo:detail", question_id=answer.question.id)
+    else:
+        form = AnswerForm(instance=answer)
+
+    context = {"answer": answer, "form": form}
+    return render(request, "pybo/answer_form.html", context)
+
+
+@login_required(login_url="common:login")
+def answer_delete(request, answer_id):
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, "삭제 권한이 없습니다.")
+    else:
+        answer.delete()
+    return redirect("pybo:detail", question_id=answer.question.id)

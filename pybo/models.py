1b9d68669455585fcb9b481e15ff9c1fdaddd82f
youngjeaMo <tony9595@naver.com>
2025-03-26 09:38:48 +0900

    commit

--- pybo/models.py
+++ pybo/models.py
@@ -8,7 +8,9 @@ from django.contrib.auth.models import User
 class Question(models.Model):
 
     # author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="author_question"
+    )
     # 필드가 null로 저장되는 것을 허용 null=True | blank=True
     subject = models.CharField(max_length=100)
     # 글자 수에 제한이 없는 텍스트는 TextField를 사용한다
@@ -17,6 +19,7 @@ class Question(models.Model):
     # modify 칼럼에 null 허용
     # blank=True 는 formis_valid()를 통한 입력 데이터 검증 시 값이 없어도 된다는 의미
     modify_date = models.DateField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name="voter_question")
 
     def __str__(self):
         return self.subject

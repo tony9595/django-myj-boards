23f4f506961d7d0fba4e4889a0f1c414c49fb9d0
youngjeaMo <tony9595@naver.com>
2025-03-26 15:22:29 +0900

    commit

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -6,19 +6,30 @@ from pybo.forms import AnswerForm, QuestionForm
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.db.models import Q
 
 
 def index(request):
-
-    print(request.user)
+    # print(request.user)
 
     page = request.GET.get("page", "1")  # 페이지
 
+    kw = request.GET.get("kw", "")
+
     question_list = Question.objects.order_by("-create_date")
+
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw)  # 질문 제목 검색
+            | Q(content__icontains=kw)  # 질문 내용 검색
+            | Q(answer__content__icontains=kw)  # 답변 내용 검색
+            | Q(author__username__icontains=kw)  # 질문 작성자 검색
+            | Q(answer__author__username__icontains=kw)  # 답변 작성자 검색
+        ).distinct()
 
     paginator = Paginator(question_list, 10)
     page_obj = paginator.get_page(page)
-    context = {"question_list": page_obj}
+    context = {"question_list": page_obj, "page": page, "kw": kw}
     # context = {"question_list": question_list}
     return render(request, "pybo/question_list.html", context)
 
